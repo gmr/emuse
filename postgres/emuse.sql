89f801eb4f0c60bb0e575d7c0fc6d2018a1020bf
Gavin M. Roy <gavinmroy@gmail.com>
2025-03-16 18:10:31 -0400

    Refactor database schema and add poetry table.
    Reorganized the database by introducing a versioned schema (v1) and updating table references accordingly. Added a new `poetry` table with fields for metadata, content, and tags, along with a privacy_level enum type for visibility settings. This improves schema structure and expands functionality.

--- postgres/emuse.sql
+++ postgres/emuse.sql
@@ -2,8 +2,16 @@ CREATE DATABASE emuse;
 
 \c emuse;
 
-CREATE TABLE accounts (
+DROP SCHEMA v1 CASCADE;
+
+CREATE SCHEMA v1;
+
+SET search_path=v1;
+
+CREATE TABLE v1.accounts (
     id             UUID PRIMARY KEY,
+    signup_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_login_at  TIMESTAMP WITH TIME ZONE,
     first_name     TEXT NOT NULL,
     surname        TEXT NOT NULL,
     display_name   TEXT NOT NULL,
@@ -18,4 +26,20 @@ CREATE TABLE accounts (
     administrator  BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE UNIQUE INDEX ON accounts (email);
+CREATE UNIQUE INDEX ON v1.accounts (email);
+
+CREATE TYPE v1.privacy_level AS ENUM ('public', 'logged-in-only', 'friends-only', 'private');
+
+CREATE TABLE IF NOT EXISTS v1.poetry (
+    id             UUID PRIMARY KEY,
+    owner          UUID REFERENCES v1.accounts (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    title          TEXT,
+    created_at     DATE  DEFAULT CURRENT_DATE,
+    posted_at      TIMESTAMP WITH TIME ZONE  DEFAULT CURRENT_TIMESTAMP,
+    language       TEXT,
+    explicit       BOOLEAN,
+    privacy_level  privacy_level  DEFAULT 'public',
+    content        TEXT,
+    notes          TEXT,
+    tags           TEXT[]
+);
